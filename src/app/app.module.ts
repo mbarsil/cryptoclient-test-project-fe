2613a3903f707ebf254100b9ff635a87cb5774f1
Mario Barrajon Silva <marb@ciklum.com>
2020-10-04 20:02:04 +0200

    setup(accounts.module): Module reorganization

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,35 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatTableModule } from '@angular/material/table';
-import { MatPaginatorModule } from '@angular/material/paginator';
 
 import { SocketIoModule } from 'ngx-socket-io';
 
 import { environment } from '../environments/environment';
 
 import { AppRoutingModule } from './app-routing.module';
+import { AccountsModule } from './accounts/accounts.module';
 
 import { AppComponent } from './app.component';
-import { AccountsComponent } from './accounts/accounts.component';
-import { AccountDetailComponent } from './accounts/account-detail/account-detail.component';
-import { MatSortModule } from '@angular/material/sort';
 import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AccountsComponent,
-    AccountDetailComponent,
     HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatTableModule,
-    MatPaginatorModule,
-    MatSortModule,
+    AccountsModule,
 
     SocketIoModule.forRoot(environment.wsConfig)
   ],
